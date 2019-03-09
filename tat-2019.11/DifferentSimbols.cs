ba38a23efa3f89244f0a847fa4cc17b748738ec1
Липская Юлия <yuliya.santotsckay@yandex.ru>
2019-03-09 21:16:04 +0300

    added

---
+++ tat-2019.11/DifferentSimbols.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TAT_2019._1
+{
+    class DifferentSimbols
+    {
+        /// <summary>
+        /// Method SearchDifferentSimbols
+        /// Function that compares the elements , and outputs sequences of non-duplicate characters.
+        /// </summary>
+        /// <param name="str"></param>
+        public void SearchDifferentSimbols(string strfromcommandline)
+        {
+            string strfordiffsimb = null; //new empty string for future sequences
+
+            for (int i = 0; i < strfromcommandline.Length; i++)
+            {
+                strfordiffsimb = "";
+                strfordiffsimb = strfordiffsimb + strfromcommandline[i];
+
+                for (int j = 0; j < strfromcommandline.Length - i - 1; j++)
+                {
+                    if (strfromcommandline[i + j] != strfromcommandline[i + 1 + j])
+                    {
+                        strfordiffsimb += strfromcommandline[i + 1 + j];
+                        Console.WriteLine(strfordiffsimb);
+                    }
+
+                    else break;
+                }
+            }
+        }
+    }
+}
--- tat-2019.11/Program.cs
+++ tat-2019.11/Program.cs
@@ -4,12 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace tat_2019._11
+namespace TAT_2019._1
 {
+    /// <summary>
+    /// the main class Program,entry point to the application.
+    ///From here we get the string and check its length.
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
+            if (args.Length != 0) // Checking the condition for the argument
+            {
+                string strfromcommandline = null;
+                foreach (string element in args)
+                {
+                    strfromcommandline += element; // rewrite items from command line to variable
+                }
+
+                DifferentSimbols obj = new DifferentSimbols();
+                obj.SearchDifferentSimbols(strfromcommandline);
+            }
+
+            else
+            {
+                Console.WriteLine("String must be longer than 1 simbol");
+            }
+
+            Console.Read();
         }
     }
 }
+
+
